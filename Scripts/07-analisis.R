975cbae20da36c93c80c359310729c85ca1a88ec
Pablo <pmbusch@uc.cl>
2020-08-17 13:09:08 -0400

    Change color pallete, Add label "normas" to graph

--- Scripts/07-analisis.R
+++ Scripts/07-analisis.R
@@ -54,36 +54,43 @@ rm(t)
 
 
 # Serie de tiempo ---------------
+# Para expandir los limites y mostrar bien los labels
+ano_max <- max(df_anual$year)
+ano_min <- min(df_anual$year)
+
 serie_tiempo <- df_diario %>% ggplot(aes(x=date,y=valor,col=tipo_dato))+
   geom_line(aes(group=site))+
   geom_hline(yintercept = norma_diaria, col="red", linetype = "dashed")+
+  # geom_text(x=as_date(paste(ano_min,"-01-01",sep="")), 
+  #           y=norma_diaria, vjust=-.5, 
+  #           label="Norma diaria del percentil 98", col="red", size=3)+
   facet_grid(site~.)+
   labs(x = "", y = label_conc, col = "Tipo Dato",
        caption="Línea punteada representa la norma diaria del percentil 98")+
   scale_y_continuous(labels=function(x) format(x,big.mark = " ", decimal.mark = ".", scientific = F))+
   scale_color_viridis_d()+xlab('')+
   scale_x_date(date_labels = "%Y", date_breaks = "1 year")
- 
+serie_tiempo
+
+
 # Tendencia mensual
 tendencia_mensual <- df_mes %>% 
   ggplot(aes(x=date,y=valor,col=site))+
   geom_line()+geom_point()+
-  geom_hline(yintercept = norma_anual, col="red", linetype = "dashed")+
-  labs(x = "", y = label_conc, col = "Estación",
-       caption="Línea punteada representa la norma anual")+
+  geom_hline(yintercept = norma_anual, col="red", linetype = "dashed",size=1)+
+  geom_text(x=as_date(paste(ano_min,"-06-01",sep="")),
+            y=norma_anual, vjust=-0.5, label="Norma anual", col="red")+
+  labs(x = "", y = label_conc, col = "Estación")+
   scale_y_continuous(labels=function(x) format(x,big.mark = " ", decimal.mark = ".", scientific = F))+
   scale_color_viridis_d()+xlab('')+
   scale_x_date(date_labels = "%Y", date_breaks = "1 year")
-
+# tendencia_mensual
 
 # Slope Graph  -------------
 
 # Filter max and min values
 df_max <- df_anual %>% group_by(site) %>% slice(which.max(year))
 df_min <- df_anual %>% group_by(site) %>% slice(which.min(year))
-# Para expandir los limites y mostrar bien los labels
-ano_max <- max(df_anual$year)
-ano_min <- min(df_anual$year)
 
 slope_graph <- df_anual %>% ggplot(aes(x = year, y = valor)) +
   geom_line(aes(group = site, color = site), size=1.5) +
@@ -111,8 +118,7 @@ slope_graph <- df_anual %>% ggplot(aes(x = year, y = valor)) +
   expand_limits(x=c(ano_min-.5,ano_max+.5))+
   scale_x_continuous(name="", breaks=ano_min:ano_max)+
   theme(legend.position = "none")+
-  labs(caption="Línea punteada representa la norma anual")
-
+  geom_text(x=ano_min,y=norma_anual, vjust=-1, label="Norma anual", col="red")
 # slope_graph
 rm(df_max, df_min, ano_max, ano_min)
 
@@ -130,8 +136,12 @@ heat_map <- df_horaMensual %>%
   ggplot( aes(x =month, y =hora, fill = valor, col=valor))+
   geom_tile(size=0.5) +
   facet_grid(year~site)+
-  scale_fill_viridis_c(option = "plasma", direction=-1, name = label_conc) +
-  scale_color_viridis_c(option = "plasma", direction=-1, name = label_conc)+
+  scale_fill_distiller(palette = "YlOrRd", type = 'seq', 
+                        na.value = "white", direction = 1,
+                        name = label_conc)+
+  scale_color_distiller(palette = "YlOrRd", type = 'seq', 
+                        na.value = "white", direction = 1,
+                        name = label_conc)+
   expand_limits(col=0, fill=0)+
   theme(axis.text.x = element_text(angle = 90),legend.title = element_text(size = 12),
         axis.line = element_blank(), axis.ticks = element_blank(),
